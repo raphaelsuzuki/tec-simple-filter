e8f47bc3f82dbfe16395465f017d26c7878af2a1
Raphael Suzuki <szkds@tuta.io>
2026-01-28 22:54:53 +0900

    Rename default branch to main and update GitUpdater header

--- tec-simple-filters.php
+++ tec-simple-filters.php
@@ -5,7 +5,7 @@
  * Version: 0.1.0
  * Author: Raphael Suzuki
  * GitHub Plugin URI: https://github.com/raphaelsuzuki/tec-simple-filter
- * Primary Branch: master
+ * Primary Branch: main
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
